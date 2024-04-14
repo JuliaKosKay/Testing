50ef372cb1ed9689aaaada9e1e09ab8d52b9b4c9
JuliaKosKay <166844087+JuliaKosKay@users.noreply.github.com>
2024-04-14 13:52:34 +0300

    tests educ

---
+++ test_order.py
@@ -0,0 +1,19 @@
+def method1(setup, set_up):
+    print('Message have been sent')
+
+def method2(set_up, setup):
+    print('Message have been sent')
+
+def method3(set_up, setup):
+    print('Message have been sent')
+
+def method4(set_up, setup):
+    print('Message have been sent')
+
+def method5(set_up, setup):
+    print('Message have been sent')
+
+def method6(set_up, setup):
+    print('Message have been sent')
+
+#pytest -s -v test_order.py
