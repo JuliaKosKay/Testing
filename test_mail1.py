2df1000e79acdc0106d1d42da9735ce0872a604d
JuliaKosKay <166844087+JuliaKosKay@users.noreply.github.com>
2024-04-14 14:00:40 +0300

    tests educ

---
+++ test_mail1.py
@@ -0,0 +1,28 @@
+import pytest
+@pytest.fixture
+
+def setup():
+    print('Authorization successfully complete')
+    yield #return gen
+    print('exit')
+
+def test_send_mail1(setup): #collecting ... collected 1 item
+    #1 passed in 0.01s
+    print('Message have been sent')
+
+def test_send_mail2(set_up):
+    print('Message have been sent')
+
+'''
+pytest -v every command testing + %
+Test_mail.py::test_send_mail1 PASSED [ 50%] 
+
+-s without percent complete
+Test_mail.py Authorization successfully complete
+Message have been sent
+
+-s - v 
+Test_mail.py::test_send_mail1 Authorization successfully complete
+Message have been sent
+PASSED
+'''
